0c913aabc5aeccce781ab23873ea9468e9e2613e
krystalwhite <krystalsnowhite@gmail.com>
2021-05-25 18:46:53 -0500

    Creating structure for steps #1 and #2 in studio

--- scripts.js
+++ scripts.js
@@ -1,2 +1,28 @@
 // Write your JavaScript code here.
 // Remember to pay attention to page loading!
+window.addEventListener("load", function() {
+    console.log("page loaded");
+    initialize();
+});
+
+function initialize () {
+    let takeoffButton = document.getElementById("takeoff");
+    let flightStatusUpdate = document.getElementById("flightStatus");
+    let flightScreenColor = document.getElementById("shuttleBackground");
+    let shuttleHeight = document.getElementById("spaceShuttleHeight");
+    let landButton = document.getElementById("landing");
+    let abortButton = document.getElementById("missionAbort");
+    let upButton = document.getElementById("up");
+    let downButton = document.getElementById("down");
+    let rightButton = document.getElementById("right");
+    let leftButton = document.getElementById("left");
+
+    takeoffButton.addEventListener("click", function() {
+        let response = window.confirm("Confirm that the shuttle is ready for takeoff.");
+        if (response) {
+            flightStatusUpdate.innerHTML = "Shuttle in flight.";
+            flightScreenColor = "blue";
+            shuttleHeight.innerHTML += 10000;
+        }
+    } 
+});
