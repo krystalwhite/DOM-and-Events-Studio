ddf6c3fd1b454ae413ddc40a96cb24cfb07e3134
krystalwhite <krystalsnowhite@gmail.com>
2021-05-27 06:37:05 -0500

    Updated all the directional buttons and the movement of image

--- scripts.js
+++ scripts.js
@@ -1,11 +1,4 @@
-// Write your JavaScript code here.
-// Remember to pay attention to page loading!
 window.addEventListener("load", function() {
-    console.log("page loaded");
-    initialize();
-});
-
-function initialize () {
     let takeoffButton = document.getElementById("takeoff");
     let flightStatusUpdate = document.getElementById("flightStatus");
     let flightScreenColor = document.getElementById("shuttleBackground");
@@ -17,48 +10,57 @@ function initialize () {
     let rightButton = document.getElementById("right");
     let leftButton = document.getElementById("left");
 
+    let imgObj = document.getElementById("shuttlepic");
+    imgObj.style.position= 'absolute'; 
+    imgObj.style.left = '150px'; 
+    imgObj.style.bottom = '0px';
+
     takeoffButton.addEventListener("click", function() {
         let response = window.confirm("Confirm that the shuttle is ready for takeoff.");
         if (response) {
             flightStatusUpdate.innerHTML = "Shuttle in flight.";
-            flightScreenColor = "blue";
-            shuttleHeight.innerHTML += 10000;
+            flightScreenColor.style.backgroundColor = "blue";
+            imgObj.style.bottom = "10px";
+            shuttleHeight.innerHTML = 10000;
         }
     }); 
 
     landButton.addEventListener("click", function() {
         window.alert("The shuttle is landing. Landing gear engaged.");
         flightStatusUpdate.innerHTML = "The shuttle has landed.";
-        flightScreenColor = "green";
+        flightScreenColor.style.backgroundColor = "green";
         shuttleHeight.innerHTML = 0;
+        imgObj.style.bottom = "0px";
+        imgObj.style.left = '150px'; 
     });  
     
     abortButton.addEventListener("click", function() {
         let response = window.confirm("Confirm that you want to abort the mission.");
         if (response) {
             flightStatusUpdate.innerHTML = "Mission aborted.";
-            flightScreenColor = "green";
+            flightScreenColor.style.backgroundColor = "green";
             shuttleHeight.innerHTML = 0;
-    });  
+            imgObj.style.bottom = "0px";
+            imgObj.style.left = '150px'; 
+        }
+    });
 
     upButton.addEventListener("click", function() {
-        // move rocket image up 10 px
-        shuttleHeight.innerHTML += 10000;
+        imgObj.style.bottom = parseInt(imgObj.style.bottom) + 10 + 'px';
+        shuttleHeight.innerHTML = parseInt(shuttleHeight.innerHTML) + 10000;
     });  
 
     downButton.addEventListener("click", function() {
-        // move rocket image down 10 px
-        shuttleHeight.innerHTML -= 10000;
+        imgObj.style.bottom = parseInt(imgObj.style.bottom) + -10 + 'px';
+        shuttleHeight.innerHTML = parseInt(shuttleHeight.innerHTML) + -10000;
     });  
 
     leftButton.addEventListener("click", function() {
-        // move rocket image left 10 px
+        imgObj.style.left = parseInt(imgObj.style.left) + -10 + 'px';
     });  
 
-    leftButton.addEventListener("click", function() {
-        // move rocket image right 10 px
+    rightButton.addEventListener("click", function() {
+        imgObj.style.left = parseInt(imgObj.style.left) + 10 + 'px';
     });  
 
 });
-
-
